8749c9c01be5ac96eaeb2ff6d0b4bf7d7f3d3214
facadmin1 <facadmin1@LTIN184811>
2020-07-14 10:55:58 +0530

    Hii

---
+++ Mohan/src/com/lover/Demo.java
@@ -0,0 +1,14 @@
+package com.lover;
+
+public class Demo {
+	public static void kk() {
+		String s = "hello world";
+		System.out.println(s);
+
+	}
+	public static void main(String[] args) {
+		kk();
+		
+	}
+	
+}

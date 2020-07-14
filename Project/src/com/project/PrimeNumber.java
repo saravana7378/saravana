ed7fc0a385980eb0505af72f21670b202c41750d
facadmin1 <facadmin1@LTIN184811>
2020-07-14 10:59:03 +0530

    jj

---
+++ Project/src/com/project/PrimeNumber.java
@@ -0,0 +1,15 @@
+package com.project;
+
+public class PrimeNumber {
+	public static void kk() {
+		String s = "hello world";
+		System.out.println(s);
+
+	}
+	public static void main(String[] args) {
+		kk();
+		
+	}
+	
+
+}
